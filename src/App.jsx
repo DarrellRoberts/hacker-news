502916d2a00f6d896455bfc0ad5112d6471739ba
hari-pace <hari.pace23@gmail.com>
2023-08-31 14:03:03 +0200

    hari updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,74 +1,133 @@
-import { useState, useEffect } from 'react'
-import './App.css'
+import { useState, useEffect } from "react";
+import "./App.css";
+import PaginatedItems from "./components/Hari";
 
 function App() {
-const [posts, setPosts] = useState({ hits: [] });
-const fetchData = async () => {
-  try {
-    const res = await fetch("https://hn.algolia.com/api/v1/search");
-    const data = await res.json();
-    setPosts(data);
-    console.log(data);
-  } catch (err) {
-    console.log(err);
-  }
-};
-useEffect(() => {
-  fetchData();
-  console.log(posts);
-}, []);
+  const [posts, setPosts] = useState({ hits: [] });
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage, setPostsPerPage] = useState(10);
+
+  const fetchData = async () => {
+    try {
+      const res = await fetch("https://hn.algolia.com/api/v1/search");
+      const data = await res.json();
+      setPosts(data);
+      console.log(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+    console.log(posts);
+  }, []);
+
+  const lastPostIndex = currentPage * postsPerPage;
+  const firstPostIndex = lastPostIndex - postsPerPage;
+  const currentPosts = posts.hits.slice(firstPostIndex, lastPostIndex);
 
   return (
     <>
-    <div className="container">
-      <header>
-        <div className="header-left">
-          <h2 className="logo">Y</h2>
-          <h1 className="hacker-news">Hacker News</h1>
-          <ul>
-          <a href=""><li>new</li></a>
-          <a href=""><li>past</li></a>
-          <a href=""><li>comments</li></a>
-          <a href=""><li>ask</li></a>
-          <a href=""><li>show</li></a>
-          <a href=""><li>jobs</li></a>
-          <a href=""><li>submit</li></a>
-          </ul>
-        </div>
-        <div className="header-right">
-          <a href=""><li>login</li></a>
-        </div>
-      </header>
-      <main>
-        {posts.hits.map((post) => (
-          <div key={post.id}>
-            <ol>
-              <a href={post.url} target="_blank" rel="noreferrer"><span>{post.story_text ? post.story_text : post.title} ({post.url})</span></a>
-              <br/><span>{post.points} points by {post.author} hide {post.num_comments} comments </span>
-            </ol>
-        </div>))}
-      </main>
-      <footer>
-        <div className="footer-links">
-        <ul>
-          <a href=""><li>Guidelines</li></a>
-          <a href=""><li>Faq</li></a>
-          <a href=""><li>Lists</li></a>
-          <a href=""><li>API</li></a>
-          <a href=""><li>Security</li></a>
-          <a href=""><li>Legal</li></a>
-          <a href=""><li>Apply to YC</li></a>
-          <a href=""><li>Contact</li></a>
-          </ul>
-        </div>
-        <div className="search-bar">
-          <label>Search:</label>
-          <input type="text" />
-        </div>
-      </footer>
+      <div className="container">
+        <header>
+          <div className="header-left">
+            <h2 className="logo">Y</h2>
+            <h1 className="hacker-news">Hacker News</h1>
+            <ul>
+              <a href="">
+                <li>new</li>
+              </a>
+              <a href="">
+                <li>past</li>
+              </a>
+              <a href="">
+                <li>comments</li>
+              </a>
+              <a href="">
+                <li>ask</li>
+              </a>
+              <a href="">
+                <li>show</li>
+              </a>
+              <a href="">
+                <li>jobs</li>
+              </a>
+              <a href="">
+                <li>submit</li>
+              </a>
+            </ul>
+          </div>
+          <div className="header-right">
+            <a href="">
+              <li>login</li>
+            </a>
+          </div>
+        </header>
+        <main>
+          {currentPosts.map((post) => (
+            <div key={post.id}>
+              <ol>
+                <a href={post.url} target="_blank" rel="noreferrer">
+                  <span>
+                    {post.story_text ? post.story_text : post.title} ({post.url}
+                    )
+                  </span>
+                </a>
+                <br />
+                <span>
+                  {post.points} points by {post.author} hide {post.num_comments}{" "}
+                  comments{" "}
+                </span>
+              </ol>
+            </div>
+          ))}
+          <div>
+            <PaginatedItems
+              totalPosts={posts.hits.length}
+              postsPerPage={postsPerPage}
+              setCurrentPage={setCurrentPage}
+              currentPage={currentPage}
+            />
+          </div>
+        </main>
+        <footer>
+          <div className="footer-links">
+            <ul>
+              <a href="">
+                <li>Guidelines</li>
+              </a>
+              <a href="">
+                <li>Faq</li>
+              </a>
+              <a href="">
+                <li>Lists</li>
+              </a>
+              <a href="">
+                <li>API</li>
+              </a>
+              <a href="">
+                <li>Security</li>
+              </a>
+              <a href="">
+                <li>Legal</li>
+              </a>
+              <a href="">
+                <li>Apply to YC</li>
+              </a>
+              <a href="">
+                <li>Contact</li>
+              </a>
+            </ul>
+          </div>
+          <div className="search-bar">
+            <label>Search:</label>
+            <input type="text" />
+          </div>
+        </footer>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
